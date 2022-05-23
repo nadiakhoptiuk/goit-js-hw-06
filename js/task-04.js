91e6a33e41c95dac3c2ce301b723b2a7b2cbfe29
Nadia Khoptiuk <holineinadia@gmail.com>
2022-05-23 23:03:44 +0300

    add task 4

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,24 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = 0;
+
+const refs = {
+  increment: document.querySelector('button[data-action="increment"]'),
+  decrement: document.querySelector('button[data-action="decrement"]'),
+  value: document.querySelector("#value"),
+};
+
+refs.increment.addEventListener("click", onIncrementButtonClick);
+refs.decrement.addEventListener("click", onDecrementButtonClick);
+
+function onDecrementButtonClick(event) {
+  counterValue -= 1;
+  refs.value.textContent = counterValue;
+}
+
+function onIncrementButtonClick(event) {
+  counterValue += 1;
+  refs.value.textContent = counterValue;
+}
